053b42c99d2fb36db1f5297c6d811b3faa357451
unknown <teddytiyi@gmail.com>
2023-06-12 22:48:41 +0200

    Add Redux in React components

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,13 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const booksSlice = createSlice({
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
+
+export const booksSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState,
   reducers: {
     addBook: (state, action) => {
+      // Add a book to the state
       state.push(action.payload);
     },
-    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
+    removeBook: (state, action) => {
+      // Remove a book from the state by id
+      const bookIndex = state.findIndex((book) => book.item_id === action.payload);
+      if (bookIndex !== -1) {
+        state.splice(bookIndex, 1);
+      }
+    },
   },
 });
 
